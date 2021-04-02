f492c1343eaaa05a2b86bd897cd2e58e2f0649b0
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2021-04-01 21:06:58 -0500

    Updated individual functions for each variable

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -10,81 +10,82 @@ var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 var numeric = ["0","1","2","3","4","5","6","7","8","9"];
 var special = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","`","~","[","{","]","}","\\","|",";",":","\'","\"",",","<",".",">","/","?"];
 
-// Write password to the #password input
-function writePassword() {
-  console.log("working"); //testing to make sure function is coded
-
-lengthOfPassword();
-  //User inputs character #
-  function lengthOfPassword() {
-    var lengthOfPassword = parseInt(prompt("Please enter how many characters are needed in the length of the password."));
-    if (lengthOfPassword > 8 && lengthOfPassword < 128){ 
+//create length   //User inputs character #
+function lengthOfP() {
+  var lengthOfPassword = parseInt(prompt("Please enter how many characters are needed in the length of the password."));
+  if (lengthOfPassword > 8 && lengthOfPassword < 128){ 
     console.log(lengthOfPassword);
     lowerCaseCase(); 
     return lengthOfPassword;
+  } else {
+    alert ("Please enter a number larger than 8 and less than 128.")
+    lengthOfP();
   }
-    else {alert ("Please enter a number larger than 8 and less than 128.")
-    lengthOfPassword();}
 }
 
-
-  //create variable for lowercase
-  function lowerCaseCase() {
-    var lower = prompt("Please select whether you would like lowercase letters in your password.");
+//create variable for lowercase //User responds whether or not they want to have characters
+function lowerCaseCase() {
+  var lower = prompt("Please select whether you would like lowercase letters in your password.");
     if (lower == "yes"){
     // randomize selection from lowerCase, math floor will pull the largest value from array 
-    lower = lowerCase[Math.floor(Math.random()*lowerCase.length)];
-    console.log(lower);
-    capital();
-    }
+      lower = lowerCase[Math.floor(Math.random()*lowerCase.length)];
+      console.log(lower);
+      capital();
+      }
     else if (lower == "no"){ 
       capital();}
     else {alert("Please type yes or no.")
   lowerCaseCase();}
 }
-  // {capital();}}
 
-
-  //create function for upperCase
-  function capital(){
-    var cap = prompt("Please select whether you would like uppercase letters in your password.");
+//create function for upperCase
+function capital(){
+  var cap = prompt("Please select whether you would like uppercase letters in your password.");
     if (cap == "yes"){
-    //randomize selection from upperCase, math floor will pull the largest value from array and math.random will work on array length to randomize selection
-    cap = upperCase[Math.floor(Math.random()*upperCase.length)];
-    console.log(cap);
-    numbers();
-  }
-  else if (cap == "no"){
-    numbers();
-  }
-  else {alert("Please type yes or no."); 
+  //randomize selection from upperCase, math floor will pull the largest value from array and math.random will work on array length to randomize selection
+      cap = upperCase[Math.floor(Math.random()*upperCase.length)];
+      console.log(cap);
+      numbers();
+      }
+    else if (cap == "no"){
+      numbers();
+  } else {alert("Please type yes or no."); 
   capital();}
 }
 
-  //create numeric
-  function numbers(){
-    var num = prompt("Please select whether you would like numbers in your password.")
+//create numeric
+function numbers(){
+  var num = prompt("Please select whether you would like numbers in your password.")
     if (num == "yes"){
-    num = numeric[Math.floor(Math.random()*numeric.length)];
-    console.log(num);
+      num = numeric[Math.floor(Math.random()*numeric.length)];
+      console.log(num);
+      schar();
+      }
+    else if (num =="no"){
     schar();
-  }
-  else if (num =="no"){
-    schar();
-  }
-else {alert("Please type yes or no.");
-numbers();}
+    } else {alert("Please type yes or no.");
+    numbers();}
   }
 
-  //create special
-  function schar(){
-    var schar = prompt("Please select whether you would like special characters in your password.")
+//create special
+function schar(){
+  var schar = prompt("Please select whether you would like special characters in your password.")
     if (schar == "yes"){
-    schar = special[Math.floor(Math.random()*special.length)];
-    console.log(schar);
-  ;}
+      schar = special[Math.floor(Math.random()*special.length)];
+      console.log(schar);
+      }
+    }
 
-    }}
+// Write password to the #password input
+function writePassword() {}
+  console.log("working"); //testing to make sure function is coded
+
+lengthOfP();
+lowerCaseCase();
+capital();
+numbers();
+schar();
+
   
 
     // return writePassword();
@@ -104,4 +105,4 @@ numbers();}
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
